edf5fefc2a5a56899c4935bd8308477e6c888fda
mdsalehin <76901299+mdsalehin@users.noreply.github.com>
2024-10-06 15:26:44 +0600

    Add files via upload

---
+++ OOPBasics/Program.cs
@@ -0,0 +1,74 @@
+﻿//object declaration in c#
+//object = (class name + varibale) = KeywordName-> (new then again class name )
+//object = each and every iteam is indepandent.like a blueprint of class akta change krle oprtar kno change nai
+
+//object declaration
+/*WaterBottle bottle1 = new WaterBottle();
+bottle1.capacity = 100;
+bottle1.color = "red";
+*/
+
+
+
+//work_with_consturctor 
+WaterBottle bottle1 = new WaterBottle("red",100);
+
+
+
+/*ayta ki hoilo ayta kisu hoilo?? addwater to thaole thaaklaoi nah 
+bottle1.waterAmount = 300;*/
+//ayta solve korar jno lagbe property feature  and saterr and gater mathod
+
+//using property but this is not varibale.
+//bottle1.Propertywateramount = 300;
+
+//mathod call
+bottle1.AddWater(30);
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+//another object
+
+/*WaterBottle bottle2 = new WaterBottle();
+bottle2.capacity = 150;
+bottle2.color = "blue";
+*/
+
+//work_with_consturctor 
+WaterBottle bottle2 = new WaterBottle("blue",150);
+
+
+
+
+
+
+//mathod call
+bottle2.AddWater(50);
+bottle2.AddWater(30);
+
+
+//print
+//Console.WriteLine(bottle1.waterAmount);
+//Console.WriteLine(bottle2.waterAmount);
+
+//print after property declaration
+Console.WriteLine(bottle1.Propertywateramount);
+Console.WriteLine(bottle2.Propertywateramount);
+
+
+
+// Print capacity and color for bottle1
+Console.WriteLine("Bottle 1:");
+Console.WriteLine("Color: " + bottle1.Color);
+Console.WriteLine("Capacity: " + bottle1.Capacity);
+Console.WriteLine("Water Amount: " + bottle1.Propertywateramount);
+
+// Print capacity and color for bottle2
+Console.WriteLine("\nBottle 2:");
+Console.WriteLine("Color: " + bottle2.Color);
+Console.WriteLine("Capacity: " + bottle2.Capacity);
+Console.WriteLine("Water Amount: " + bottle2.Propertywateramount);
+
+
---
+++ OOPBasics/WaterBottle.cs
@@ -0,0 +1,96 @@
+﻿//Encapsulation
+//Access Modifier
+//class toiri kora + access modifier ay 2tar mdhme encapsulation kora hoye thake.
+//Encapsulation =  Class + Access Modifier.
+
+//Access Modifier = private,protected,internal,public
+
+class WaterBottle 
+{
+    private double capacity; //variable hoche = ki ache agolo ke consider krbo variable hisabe.
+    private string color;   //variable hoche = information ba data.
+    private double waterAmount;
+
+    //decalaration Property avoid confilt of program.cs ar addwater to thaole thaaklaoi nah ayline
+
+    // Property decalaration 
+    public double Propertywateramount
+    {
+       // propety kintu data hold krtase nah data hold krtase (waterAmount)
+        get
+        {
+            //deacler varibale
+            return waterAmount;
+        }
+       private set
+        {
+            //aykhne wateramount ar value ta set krchi.
+            if (value <= capacity)
+            {
+                waterAmount = value;
+            }
+
+        }
+
+    }
+
+    /* property means = mathod e mne korbo.property is one
+    kinds of mathod.diffrence is there is no paramitter */
+
+
+
+
+    //to_know_color_and_capacity_how_much_we_use another property
+
+    public string Color
+    {
+        get
+        {
+            return color;
+        }
+    }
+    public double Capacity
+    {
+        get
+        {
+            return capacity;
+        }
+    }
+
+
+    //Constructor has no return type
+    //Now Work With Constructor
+
+  /*  class name and no return Type no another name allow on constructor.
+        allow only class name in construtor*/
+
+   // consturctor declearation
+    public WaterBottle(string color, double ToAvoidThisWeUseDifferentNameOFCapacity)
+    {
+        this.color = color;
+        capacity = ToAvoidThisWeUseDifferentNameOFCapacity;
+    }
+
+
+
+    public void AddWater(double amount) //mahtod hoche = ki korte pare.Action bujai mathod ke.
+    {
+        if(Propertywateramount + amount <= capacity)
+        {
+            Propertywateramount = Propertywateramount + amount;
+        }       
+    }
+
+    public void ReleaseWater(double amount)
+    {
+        if(Propertywateramount < amount)
+        {
+            Propertywateramount = 0;
+        }
+        else
+        {
+            Propertywateramount -= amount;
+        }  
+    }
+
+}
