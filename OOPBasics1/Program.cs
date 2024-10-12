cdc174beaa4368ddc79ee99b7d162b002adb1b3c
mdsalehin <76901299+mdsalehin@users.noreply.github.com>
2024-10-12 14:25:17 +0600

    Add files via upload

---
+++ OOPBasics1/Program.cs
@@ -0,0 +1,28 @@
+﻿//declear object = class name + varibale  =  new agin class name ()
+WaterBottle1 bottle1 = new WaterBottle1();
+
+//real life or world ecch and every object is independent
+
+//another object
+WaterBottle1 bottle2 = new WaterBottle1();
+
+//acces class element by object name dot
+
+bottle1.capacity = 100;
+bottle1.color = "red";
+
+bottle2.capacity = 150;
+bottle2.color = "blue";
+
+
+//mathod access now
+
+bottle1.AddWater(30);
+
+bottle2.AddWater(50);
+bottle2.AddWater(30);
+
+//print
+
+Console.WriteLine(bottle1.waterAmount);
+Console.WriteLine(bottle2.waterAmount);
---
+++ OOPBasics1/WaterBottle1.cs
@@ -0,0 +1,34 @@
+﻿//Encapsulation
+//Access Modifier
+//class toiri kora + access modifier ay 2tar mdhme encapsulation kora hoye thake.
+//Encapsulation =  Class + Access Modifier.
+//Access Modifier = private,protected,internal,public
+
+
+class WaterBottle1
+{
+    public double capacity;
+    public string color;
+    public double waterAmount;
+
+    public void AddWater(double amount)
+    {
+        if (waterAmount + amount <= capacity)
+        {
+            waterAmount = waterAmount + amount;
+        }       
+    }
+    public void ReleaseWater(double amount)
+    {
+        if(waterAmount < amount)
+        {
+            waterAmount = 0;
+        }
+        else
+        {
+            waterAmount = waterAmount - amount;
+        }       
+        
+    }
+
+}
