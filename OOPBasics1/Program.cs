3279d9650e4ca5b7b4a57cc8d04491b15502ae57
mdsalehin <76901299+mdsalehin@users.noreply.github.com>
2024-10-12 16:48:55 +0600

    Add files via upload

--- OOPBasics1/Program.cs
+++ OOPBasics1/Program.cs
@@ -1,9 +1,10 @@
-﻿//declear object = class name + varibale  =  new agin class name ()
+﻿//real life or world ecch and every object is independent
+//declear object = class name + varibale  =  new agin class name ()
+
 WaterBottle1 bottle1 = new WaterBottle1();
 
-//real life or world ecch and every object is independent
-
 //another object
+
 WaterBottle1 bottle2 = new WaterBottle1();
 
 //acces class element by object name dot
@@ -14,6 +15,18 @@ bottle1.color = "red";
 bottle2.capacity = 150;
 bottle2.color = "blue";
 
+//this is not make sense capacity = 100; but water amount =300 ayjno property feature use kre thaki
+//bottle1.waterAmount = 330;
+
+//Using Property
+
+bottle1.Propertywateramount = 300;
+
+/*if bottle1.Propertywateramount = 100;<=100 then it was change the value
+    this one is not the write things is only example purpose */ 
+
+
+
 
 //mathod access now
 
@@ -22,7 +35,7 @@ bottle1.AddWater(30);
 bottle2.AddWater(50);
 bottle2.AddWater(30);
 
-//print
+//print Property
 
-Console.WriteLine(bottle1.waterAmount);
-Console.WriteLine(bottle2.waterAmount);
+Console.WriteLine(bottle1.Propertywateramount);
+Console.WriteLine(bottle2.Propertywateramount);
--- OOPBasics1/WaterBottle1.cs
+++ OOPBasics1/WaterBottle1.cs
@@ -1,15 +1,34 @@
-﻿//Encapsulation
-//Access Modifier
-//class toiri kora + access modifier ay 2tar mdhme encapsulation kora hoye thake.
-//Encapsulation =  Class + Access Modifier.
-//Access Modifier = private,protected,internal,public
-
+﻿
 
 class WaterBottle1
 {
+    //variables
     public double capacity;
     public string color;
-    public double waterAmount;
+    private double waterAmount;
+  
+    /*  property means = mathod e mne korbo.property is one
+        kinds of mathod.diffrence is there is no paramitter    
+        To avoid Conflict we are using Property Feature So its one kind of varibale but
+        diffrence is Property 1ST later is Capital */
+
+
+    //using Property
+    public double Propertywateramount
+    {
+        get
+        {
+            return waterAmount;
+        }
+        set
+        {
+            if(value <= capacity )
+            {
+                waterAmount = value;
+            }            
+        }
+    }
+  
 
     public void AddWater(double amount)
     {
