3a8353607d6875a0656daa1a5c89bd0235652785
mdsalehin <76901299+mdsalehin@users.noreply.github.com>
2024-10-14 04:17:58 +0600

    Add files via upload
    NameSpace Convention

---
+++ Namespace/Program.cs
@@ -0,0 +1,30 @@
+﻿//Accesing Namespace
+using OOP.Basisc;
+
+WaterBottle1 bottle1 = new WaterBottle1("red", 100);
+
+//another object
+WaterBottle1 bottle2 = new WaterBottle1("blue", 150);
+
+//mathod access now
+bottle1.AddWater(30);
+bottle2.AddWater(50);
+bottle2.AddWater(30);
+
+//print Property
+Console.WriteLine("Water Amount:");
+Console.WriteLine(bottle1.Propertywateramount);
+Console.WriteLine(bottle2.Propertywateramount);
+Console.WriteLine();
+
+// Print capacity and color for bottle1
+Console.WriteLine("Bottle 1:");
+Console.WriteLine("Color: " + bottle1.Propertycolor);
+Console.WriteLine("Capacity: " + bottle1.Capacity);
+Console.WriteLine("Water Amount: " + bottle1.Propertywateramount);
+
+// Print capacity and color for bottle2
+Console.WriteLine("\nBottle 2:");
+Console.WriteLine("Color: " + bottle2.Propertycolor);
+Console.WriteLine("Capacity: " + bottle2.Capacity);
+Console.WriteLine("Water Amount: " + bottle2.Propertywateramount);
---
+++ Namespace/WaterBottle1.cs
@@ -0,0 +1,89 @@
+﻿
+//using namespace concept
+namespace OOP.Basisc
+{
+    //copanyName Porject/organization/gropingtype
+    //akn class name OOP.WaterBottle1 Hoye gase Mouse Hobar krleai daka jabe.
+    class WaterBottle1
+    {
+        //variables
+        private double capacity;
+        private string color;
+        private double waterAmount;
+
+        //Apply Auto Property
+        public double Weight { get; set; }
+
+        //using Property
+        public double Propertywateramount
+        {
+            get
+            {
+                return waterAmount;
+            }
+            //Adding Access Modifier private 
+            private set
+            {
+                if (value <= capacity)
+                {
+                    waterAmount = value;
+                }
+            }
+        }
+
+        //To Knew the Color and Capacity How Much We Use Another Property
+        public string Propertycolor
+        {
+            get
+            {
+                return color;
+            }
+        }
+        public double Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+        //Constructor
+        public WaterBottle1(string color, double ToAvoidThisWeUseDifferentNameOFContructorcapacity)
+        {
+            this.color = color;
+            capacity = ToAvoidThisWeUseDifferentNameOFContructorcapacity;
+        }
+
+
+        //Disctructor Concept
+        //in c# we call it aslo finalizar. so there is no parameter in Disctructor.
+        //finalizer always e public dhore nite hobe there is no access modifier
+
+        //This is called Distructor
+        ~WaterBottle1()
+        {
+
+        }
+        public void AddWater(double amount)
+        {
+            //akn set class ar vitor diya acces krte parbo but bahire thake ar use krte parbo nah.acces modifier ar jno
+
+            if (Propertywateramount + amount <= capacity)
+            {
+                Propertywateramount = Propertywateramount + amount;
+            }
+        }
+        public void ReleaseWater(double amount)
+        {
+            if (Propertywateramount < amount)
+            {
+                Propertywateramount = 0;
+            }
+            else
+            {
+                Propertywateramount = Propertywateramount - amount;
+            }
+
+        }
+    }
+}
