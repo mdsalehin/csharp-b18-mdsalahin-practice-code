5679e2331969e6a4624169899f67a213e4db1834
mdsalehin <76901299+mdsalehin@users.noreply.github.com>
2024-10-14 04:16:04 +0600

    Add files via upload
    simple auto Property showing

---
+++ OopAutoProperty/Program.cs
@@ -0,0 +1,31 @@
+﻿//real life or world ecch and every object is independent
+//declear object = class name + varibale  =  new agin class name ()
+
+//so this in now constructor now  WaterBottle1 this is constructor 
+WaterBottle1 bottle1 = new WaterBottle1("red", 100);
+
+//another object
+WaterBottle1 bottle2 = new WaterBottle1("blue", 150);
+
+//mathod access now
+bottle1.AddWater(30);
+bottle2.AddWater(50);
+bottle2.AddWater(30);
+
+//print Property
+Console.WriteLine("Water Amount:");
+Console.WriteLine(bottle1.Propertywateramount);
+Console.WriteLine(bottle2.Propertywateramount);
+Console.WriteLine();
+
+// Print capacity and color for bottle1
+Console.WriteLine("Bottle 1:");
+Console.WriteLine("Color: " + bottle1.Propertycolor);
+Console.WriteLine("Capacity: " + bottle1.Capacity);
+Console.WriteLine("Water Amount: " + bottle1.Propertywateramount);
+
+// Print capacity and color for bottle2
+Console.WriteLine("\nBottle 2:");
+Console.WriteLine("Color: " + bottle2.Propertycolor);
+Console.WriteLine("Capacity: " + bottle2.Capacity);
+Console.WriteLine("Water Amount: " + bottle2.Propertywateramount);
---
+++ OopAutoProperty/WaterBottle1.cs
@@ -0,0 +1,71 @@
+﻿class WaterBottle1
+{
+    //variables
+    private double capacity;
+    private string color;
+    private double waterAmount;
+
+    //Apply Auto Property   so this is using  Auto Property this is just for an example. 
+    public double Weight { get; set; }
+
+    //using Property
+    public double Propertywateramount
+    {
+        get
+        {
+            return waterAmount;
+        }
+        //Adding Access Modifier private 
+        private set
+        {
+            if (value <= capacity)
+            {
+                waterAmount = value;
+            }
+        }
+    }
+
+    //To Knew the Color and Capacity How Much We Use Another Property
+    public string Propertycolor
+    {
+        get
+        {
+            return color;
+        }
+    }
+    public double Capacity
+    {
+        get
+        {
+            return capacity;
+        }
+    }
+
+    //Constructor
+    public WaterBottle1(string color, double ToAvoidThisWeUseDifferentNameOFContructorcapacity)
+    {
+        this.color = color;
+        capacity = ToAvoidThisWeUseDifferentNameOFContructorcapacity;
+    }
+    public void AddWater(double amount)
+    {
+        //akn set class ar vitor diya acces krte parbo but bahire thake ar use krte parbo nah.acces modifier ar jno
+
+        if (Propertywateramount + amount <= capacity)
+        {
+            Propertywateramount = Propertywateramount + amount;
+        }
+    }
+    public void ReleaseWater(double amount)
+    {
+        if (Propertywateramount < amount)
+        {
+            Propertywateramount = 0;
+        }
+        else
+        {
+            Propertywateramount = Propertywateramount - amount;
+        }
+
+    }
+}
